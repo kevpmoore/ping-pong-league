e34014bffabf953d1e22b6c155e9f2d51a3dfe94
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-10-07 17:02:07 +0100

    refactor factory to service

--- app/js/getPlayerDetails.js
+++ app/js/getPlayerDetails.js
@@ -1,13 +1,26 @@
-leagueTable.factory('GetPlayerDetails', ['$http', function($http) {
+leagueTable.service('GetPlayerDetails', ['$http', function($http) {
 
   var promise;
 
-  return{
-    get: function(){
-      if (!promise){
-        promise =  $http.get("../data/playerData.json");
-      }
-      return promise;
-    },
+  this.get = function() {
+    if (!promise) {
+      promise =  $http.get("../data/playerData.json");
+    }
+    return promise;
   };
+
 }]);
+
+// leagueTable.factory('GetPlayerDetails', ['$http', function($http) {
+//
+//   var promise;
+//
+//   return {
+//     get: function() {
+//       if (!promise) {
+//         promise =  $http.get("../data/playerData.json");
+//       }
+//       return promise;
+//     },
+//   };
+// }]);
